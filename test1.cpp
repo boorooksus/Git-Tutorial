4f8a82e21d3c2f460bd8165ac481e3f0f2c736ff
boorooksus <boorooksus@gmail.com>
2019-10-07 08:32:15 +0900

    Add test1.cpp [Add]

---
+++ test1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main() {
+	int a = 9;
+	int b = sqrt(10);
+	int c = sqrt(15);
+	cout << "root 9 : " << sqrt(a) << endl
+		<< "root 10 : " << b << endl
+		<< "root 15 : " << c << endl;
+	return 0;
+}
