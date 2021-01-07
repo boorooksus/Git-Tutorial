e0b240e9c960db77fd83a1d1fc0388173d70c12d
boorooksus <boorooksus@gmail.com>
2021-01-07 16:04:13 +0900

    test

---
+++ test1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main() {
+	int a = 9;
+	int b = sqrt(10);
+	int c = sqrt(15);
+	cout << "root 9 : " << sqrt(a) << endl
+		<< "root 10 : " << b << endl
+		<< "root 15 : " << c << endl;
+	cout << "hi\n";
+	return 0;
+}
